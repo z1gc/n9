05d492c2d8c13692344329b99318dd1dec9c5d36
Zigit Zo <ptr@ffi.fyi>
2025-01-03 00:38:47 +0800

    feat(unstable): nixos

---
+++ README.md
@@ -0,0 +1,15 @@
+# Unstable
+
+.files of mine, using NixOS.
+
+# .*
+
+Managed by [comtrya](https://github.com/comtrya/comtrya),
+
+```bash
+# Defaults to /
+./apply.sh evil
+
+# Aha, new stuff
+./apply.sh evil /mnt
+```
---
+++ apply.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ue
+
+MACHINE="${1:-}"
+ROOT="${2:-}"
+ROOT="${ROOT%/}"
+
+if [[ "$MACHINE" == "" ]]; then
+    echo "$0 MACHINE [ROOT]"
+    exit 1
+fi
+
+# Check comtrya:
+if [[ ! -f comtrya ]]; then
+    curl -fsSL https://get.comtrya.dev | sed 's/$BINLOCATION/./g' | bash || true
+    if ! ./comtrya --version; then
+        rm -f ./comtrya
+        echo "wrong binary, please retry..."
+        exit 1
+    fi
+fi
+
+# Generate config:
+cat > Comtrya.yaml <<EOF
+variables:
+  root: "$ROOT"
+EOF
+
+# Apply!
+sudo ./comtrya -v -d nixos apply -m "$MACHINE"
